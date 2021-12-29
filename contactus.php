dbecac09fa14e0db3723051fe6fb23ca967b8d09
meljunbalon <95677990+meljunbalon@users.noreply.github.com>
2021-12-30 01:50:33 +0800

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,98 @@
+
+<html>
+
+  <head>
+    <title> Contact | Heroes' Cafe </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/contactus.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+
+    <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+  
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Heroes' Cafe</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li ><a href="index.php">Home</a></li>
+            <li ><a href="menu.php">Menu</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li class="active"><a href="contactus.php">Contact Us</a></li>
+          </ul>
+
+
+<ul class="nav navbar-nav navbar-right">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> Sign Up <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a> User Sign-up</a></li>
+              <li> <a> Manager Sign-up</a></li>
+            
+            </ul>
+            </li>
+
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+              <li> <a> User Login</a></li>
+              <li> <a> Manager Login</a></li>
+              
+            </ul>
+            </li>
+          </ul>
+
+
+        </div>
+
+      </div>
+    </nav>
+  
+    <div class="item">
+      <img src="images/heroescafecontact.gif" style="width:100%;">
+      <div class="carousel-caption"> </div>
+
+      <br>
+      <div class="item">
+      <img src="images/heroescafedetails.gif" style="width:100%;">
+      <div class="carousel-caption"> </div>
+    
+
+     </body>
+
+  
+</html>
---
+++ menu.php
@@ -0,0 +1,109 @@
+
+<html>
+
+  <head>
+    <title> Home | Heroes' Cafe </title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+  <link rel="stylesheet" type = "text/css" href ="css/index.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+  <button onclick="topFunction()" id="myBtn" title="Go to top">
+      <span class="glyphicon glyphicon-chevron-up"></span>
+    </button>
+    <script type="text/javascript">
+      window.onscroll = function()
+      {
+        scrollFunction()
+      };
+
+      function scrollFunction(){
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          document.getElementById("myBtn").style.display = "block";
+        } else {
+          document.getElementById("myBtn").style.display = "none";
+        }
+      }
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    </script>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Heroes' Cafe</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li class="active" ><a href="index.php">Home</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li><a href="contactus.php">Contact Us</a></li>
+
+          </ul>
+
+         
+
+        
+                     <ul class="nav navbar-nav navbar-right">
+                    </ul>
+           
+
+          <ul class="nav navbar-nav navbar-right">
+                      <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Sign Up <span class="caret"></span> </a>
+                          <ul class="dropdown-menu">
+                        <li> <a> User Sign-up</a></li>
+                        <li> <a> Manager Sign-up</a></li>
+
+                      </ul>
+                      </li>
+
+                      <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                        <li> <a> User Login</a></li>
+                        <li> <a> Manager Login</a></li>
+
+                      </ul>
+                      </li>
+                    </ul>
+
+          
+       </div>
+
+      </div>
+    </nav>
+
+    <div class="item">
+      <img src="images/heroescafemenu.gif" style="width:100%;">
+      <div class="carousel-caption"> </div>
+
+    <br>
+    <div class="item">
+      <img src="images/menulist.png" style="width:100%;">
+      <div class="carousel-caption"> </div>
+
+    <div>
+      <br>
+   <center><a class="btn btn-success btn-lg" href="customerlogin.php" role="button" > Order Now </a></center>
+    </div>
+    <br>
+
+
+
+
+</body>
+</html>
